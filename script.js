a52d1f9cac030438993c51d7300de73c797c20e9
Kundan Shakya <kundanshakya023@gmail.com>
2025-03-29 07:04:41 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
-const apiUrl =  "https://cors-anywhere.herokuapp.com/https://api.quotable.io/random";
+const apiUrl =   "https://api.quotable.io/random";
 
 async function getQuote(url) {
     try {
-        const response = await fetch(url);
+        const response = await fetch(url, { mode: "no-cors" });
         const data = await response.json();
         console.log(data);
         quote.innerHTML = data.content;
